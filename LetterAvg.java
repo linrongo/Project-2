40fbfef642e0eb2f2996d89a89b4fcda4876f8c0
lin Ngo <lin.r.ngo-1@ou.edu>
2019-10-04 13:30:09 -0500

    update and test LetterAvg

--- LetterAvg.java
+++ LetterAvg.java
@@ -2,23 +2,29 @@ import java.io.IOException;
 
 public class LetterAvg extends PosAvg{
 	private char letter;
+	private String sameFirstLetterList;
 	public LetterAvg(char letter) throws IOException {
 		super(letter);
 		this.letter = letter;
+		sameFirstLetterList = "";
 	}
 	
 	public int numberOfStationWithLetterAvg() {
+		
 		int count = 0;
 		for (int i = 0; i < placement; ++i) {
 			if(stidList[i].charAt(0)==letter) {
+				sameFirstLetterList += stidList[i] + " \n";
 				++count;
 			}
 		}
 		return count;
 	}
-
+@Override
+public String toString() {
 	
-
+	return "They are: \n" + sameFirstLetterList;
+}
 	
 
 }
