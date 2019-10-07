a5592925ece9c88fb22c22d3b7d2bbdf2dde3da7
lin Ngo <lin.r.ngo-1@ou.edu>
2019-10-07 14:09:02 -0500

    update  letterAvg to satisfy zyBook

--- LetterAvg.java
+++ LetterAvg.java
@@ -2,11 +2,9 @@ import java.io.IOException;
 
 public class LetterAvg extends PosAvg{
 	private char letter;
-	private String sameFirstLetterList;
 	public LetterAvg(char letter) throws IOException {
 		super(letter);
 		this.letter = letter;
-		sameFirstLetterList = "";
 	}
 	
 	public int numberOfStationWithLetterAvg() {
@@ -14,16 +12,24 @@ public class LetterAvg extends PosAvg{
 		int count = 0;
 		for (int i = 0; i < placement; ++i) {
 			if(stidList[i].charAt(0)==letter) {
-				sameFirstLetterList += "\n" + stidList[i];
 				++count;
 			}
 		}
 		return count;
 	}
-
+	
+	 public String getStationsWithSameLetter() {
+		 String list ="";
+				for (int i = 0; i < placement; ++i) {
+				if(stidList[i].charAt(0)==letter) {
+					list += "\n" + stidList[i];				
+				}
+			}
+		 return list;
+	 }
 public String toString() {
 	
-	return "\nThey are:" + sameFirstLetterList;
+	return "\nThey are:" + getStationsWithSameLetter();
 }
 	
 
