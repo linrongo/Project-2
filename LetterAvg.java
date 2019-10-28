98f2f161b7bb4885a440e5fd32d8a34cea3a9250
lin Ngo <lin.r.ngo-1@ou.edu>
2019-10-28 13:38:02 -0500

    Update LetterAvg.java

--- LetterAvg.java
+++ LetterAvg.java
@@ -8,8 +8,8 @@ public class LetterAvg extends PosAvg{
 	}
 	
 	public int numberOfStationWithLetterAvg() {
-		
 		int count = 0;
+	// go through the list and see how many station start with the same letter of the input.
 		for (int i = 0; i < placement; ++i) {
 			if(stidList[i].charAt(0)==letter) {
 				++count;
@@ -20,6 +20,7 @@ public class LetterAvg extends PosAvg{
 	
 	 public String getStationsWithSameLetter() {
 		 String list ="";
+		 // do the same thing with numberOfStationWithLetterAvg(), but put it in a String 
 				for (int i = 0; i < placement; ++i) {
 				if(stidList[i].charAt(0)==letter) {
 					list += "\n" + stidList[i];				
